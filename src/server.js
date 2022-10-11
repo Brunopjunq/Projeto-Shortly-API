d2a6105b1dddde1e5e97dd32b8db0f94ef47da95
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-10-11 13:52:58 -0300

    Iniciando Projeto

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//app.use(router);
+
+app.get('/status', (req,res) => {
+    res.send('ok');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`);
+});
