eae84fed89671262462ea09d63c53df78b2e1e5d
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-10-14 14:42:41 -0300

    Add authController

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { postUser } from "../controllers/authController.js";
+import { validateSchema } from "../middleware/validateSchema.js";
+import authSchema from "../schemas/authSchema.js";
 
 const authRouter = Router();
 
-//authRouter.post('/signup', VALIDAÇÃO, CONTROLLER);
+authRouter.post('/signup', validateSchema(authSchema), postUser)
 //authRouter.post('/signin', VALIDAÇÃO, CONTROLLER);
 
 export default authRouter;
